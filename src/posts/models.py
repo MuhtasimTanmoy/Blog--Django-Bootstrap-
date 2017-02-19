0a6ff108b5904a71b808476a27e747faad711805
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2017-02-20 01:54:05 +0600

    1st phase Done

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,19 +1,29 @@
 from __future__ import unicode_literals
+from django.conf import settings
 
 from django.db import models
+from django.utils import timezone
 
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 # Create your models here.
 
+class PostManager(models.Manager):
+    def active(self,*args,**kwargs):
+        return super(PostManager,self).filter(draft=False).filter(publish__lte=timezone.now())
+
+
 def upload_location(instance,filename):
     filebase,extension=filename.split(".")
     return "%s/%s.%s"%(instance.id,instance.id,extension)
 
 class Post(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,default=1)
     title=models.CharField(max_length=120)
     slug=models.SlugField(unique=True)
+    draft=models.BooleanField(default=False)
+    publish=models.DateField(auto_now=False,auto_now_add=False)
     content=models.TextField()
     image=models.ImageField(upload_to=upload_location,null=True,blank=True,
     width_field="width_field",
@@ -22,6 +32,8 @@ class Post(models.Model):
     width_field=models.IntegerField(default=0)
     updated=models.DateTimeField(auto_now=True,auto_now_add=False)
     timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
+
+    objects=PostManager();
 
     def __unicode__(self):
         return self.title
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,18 +1,31 @@
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,Http404
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from urllib import quote_plus
+from django.db.models import Q
+from django.utils import timezone
 
 from .models import Post
 
 from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    queryset_list=Post.objects.all()#.order_by("-timestamp")
-    paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
+    today=timezone.now().date()
+    queryset_list=Post.objects.active()#.order_by("-timestamp")
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list=Post.objects.all()
+    query=request.GET.get("q")
+    if query:
+        queryset_list=queryset_list.filter(Q(title__icontains=query)|
+        Q(content__icontains=query)
+        ).distinct()
+    paginator = Paginator(queryset_list,4) # Show 25 contacts per page
     page_request_var="page"
+
     page = request.GET.get(page_request_var)
+
     try:
         queryset = paginator.page(page)
     except PageNotAnInteger:
@@ -25,15 +38,20 @@ def post_list(request):
     context={
     "object_list":queryset,
     "title":"list",
-    "page_request_var" : page_request_var
+    "page_request_var" : page_request_var,
+    "today":today
     }
     return render(request,"index.html",context)
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    # request.user.is_authenticated
     form=PostForm(request.POST or None,request.FILES or None)
     if form.is_valid():
         instance=form.save(commit=False)
+        instance.user=request.user
         instance.save()
         messages.success(request,"Successfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -46,6 +64,8 @@ def post_create(request):
     return render(request,"post_form.html",context)
 
 def post_update(request,slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
 
     instance=get_object_or_404(Post,slug=slug )
 
@@ -68,14 +88,22 @@ def post_update(request,slug=None):
 def post_retrieve(request,slug):
 
     instance=get_object_or_404(Post,slug=slug)
+    if instance.draft or instance.publish>timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+
+    share_string=quote_plus(instance.content)
     context={
     "title":instance.title,
-    "instance":instance
+    "instance":instance,
+    "share_string":share_string,
     }
     return render(request,"post_detail.html",context)
 
 
 def post_delete(request,slug):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance=get_object_or_404(Post,id=id)
     instance.delete()
 
